6a8f5705bc23930c5e7c0111ab7755d2c5bc2e00
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-06-19 16:09:30 +0200

    Add PDB-REDO extaction example

--- scripts/ppi_extractor.py
+++ scripts/ppi_extractor.py
@@ -1,7 +1,13 @@
-"""Example
-python scripts/extract_ppis_in_parallel.py \
-    --in_dir=/scratch/project/open-26-23/antonb/PPIRef/data/pdb \
-    --out_dir=/scratch/project/open-26-23/antonb/PPIRef/data/ppiref/ppi
+"""Examples:
+
+python scripts/ppi_extractor.py \
+    --in_dir=/scratch/project/open-26-23/antonb/PPIRef/ppiref/data/pdb \
+    --out_dir=/scratch/project/open-26-23/antonb/PPIRef/ppiref/data/ppiref/ppi_10A
+
+python scripts/ppi_extractor.py \
+    --in_dir=/scratch/project/open-26-23/antonb/PPIRef/ppiref/data/pdb-redo \
+    --out_dir=/scratch/project/open-26-23/antonb/PPIRef/ppiref/data/ppiref/pdb_redo_ppi_10A \
+    --in_file_pattern='^[a-zA-Z0-9]{4}_final_tot\.pdb$'
 """
 import os
 from pathlib import Path
@@ -26,9 +32,11 @@ from ppiref.definitions import *
 @click.option('--partition_beg', type=float, default=0.)
 @click.option('--partition_end', type=float, default=1.)
 @click.option('--input_format', type=str, default="pdb")
+@click.option('--in_file_pattern', type=str, default="*.pdb")
+@click.option('--verbose', is_flag=True)
 def main(
-    in_dir, out_dir, kind, radius, expansion_radius, bsa, join,
-    nest_out_dir, max_workers, chunk_size, partition_beg, partition_end, input_format
+    in_dir, out_dir, kind, radius, expansion_radius, bsa, join, nest_out_dir, max_workers,
+    chunk_size, partition_beg, partition_end, input_format, in_file_pattern, verbose
 ):
     ppi_extractor = PPIExtractor(
         out_dir=out_dir,
@@ -40,12 +48,13 @@ def main(
         nest_out_dir=nest_out_dir,
         max_workers=max_workers,
         chunk_size=chunk_size,
-        verbose=True,
+        verbose=verbose,
         input_format=input_format
     )
     ppi_extractor.extract_parallel(
         in_dir=in_dir,
-        partition=(partition_beg, partition_end)
+        partition=(partition_beg, partition_end),
+        in_file_pattern=in_file_pattern
     )
 
 
